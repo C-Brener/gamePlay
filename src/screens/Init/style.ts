8f48a816eed0064c9a754984830539f3152a321f
brenermr <brenercaique0806@gmail.com>
2021-12-10 05:27:29 -0300

    gamePlay

--- src/screens/Init/style.ts
+++ src/screens/Init/style.ts
@@ -1,12 +1,12 @@
 import styled from "styled-components/native";
-// import { RFValue } from "react-native-responsive-fontsize";
+import { RFValue } from "react-native-responsive-fontsize";
 
 export const Container = styled.View`
     flex:1;
     background-color: ${({theme})=>theme.colors.background};
 `;
 export const BackImage = styled.Image`
-    align-items:center;
+    align-items: center;
     
 `
 export const InfoGame=styled.View``
